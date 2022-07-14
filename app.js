87823585a23aa0ee7e2e0a287dd8606dc45a34a9
Garrett McQuigg <garrettmcquigg@gmail.com>
2022-07-14 17:28:25 -0500

    1

---
+++ app.js
@@ -0,0 +1,17 @@
+// 1. Hyper Text Transfer Protocol. How someone sends or receives data.
+
+// 2. Uniform Resource Locator which is the destination of a website.
+
+// 3. Domain Name System. Converts URL's into IP addresses.
+
+// 4. Query string uses keys and values.
+
+// 5. GET and POST. GET is used to retrieve data while POST is used to make changes to existing data or data being "posted" for the first time.
+
+// 6. Request for data from client to server.
+
+// 7. Data requested is given to client from server.
+
+// 8. Headers are information given about the request. Examples for requests are cookies and hosts while respones include Cache and content type.
+
+// 9. Clients request information from a site, that site retrieves information from the server, the server communicates with the database to access private information and returns it to the server, which then finally displays for the client.
